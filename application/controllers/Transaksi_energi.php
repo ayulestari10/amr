9dc9c0619825c9042a81c0583e70089cd71f4dff
ayulestari10 <lestariayu669@gmail.com>
2018-12-26 23:31:00 +0700

    init

---
+++ application/controllers/Transaksi_energi.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Transaksi_energi extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->module = 'transaksi_energi';
+	}
+
+	public function index()
+	{
+		$this->data['title']	= 'Dashboard';
+		$this->data['content']	= 'dashboard';
+		$this->template($this->data, $this->module);
+	}
+
+	public function target_operasi()
+	{
+		$this->data['title']	= 'Target Operasi';
+		$this->data['content']	= 'target_operasi';
+		$this->template($this->data, $this->module);
+	}
+
+	public function tambah_target_operasi()
+	{
+		$this->data['title']	= 'Tambah Target Operasi';
+		$this->data['content']	= 'tambah_target_operasi';
+		$this->template($this->data, $this->module);
+	}
+}
